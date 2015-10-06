36b1ecbfda5ef1021a605673d529cca292d65824
Aleksandr Shulman <aleks@aleks-MBP.local>
2015-10-05 21:06:56 -0700

    Initial commit of minimum viable KV store with basic tests

---
+++ src/main/java/TransactionalKVStore.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A class designed to group operations into transactions and execute those transactions.
+ * It should give the classic ACID properties:
+ * 1. Atomic - All mutations and reads in a transaction are carried out to completion, or not at all
+ * 2. Consistent - The values that the transaction depends on will not change
+ * 3. Isolated - Every action is led to believe it is the only one executing. Nothing can change the state of the system underneath it.
+ * 4. Durable - Nothing is written to disk. We'll call memory durable. But we could easily just write to a WAL.
+ *
+ * @param <K>
+ * @param <V>
+ */
+
+/**
+ * Some common cases:
+ * <p/>
+ * 1. Q: What if a transaction is started that includes a key that is already being processed?
+ * A: It won't be enqueued until that other transaction is completed, so the values it read/see
+ * will change before it starts, but at least they will not change over the duration
+ */
+public class TransactionalKVStore<K, V> {
+
+
+    //Store is source of truth. It's the backing store.
+    private final Map<K, V> store;
+
+    //This is the list of transactions that need to be processed
+    Map<Integer, List<TransactionalUnit<K, V>>> transactionQueue;
+
+    {
+        store = new HashMap<K, V>();
+        transactionQueue = new HashMap<Integer, List<TransactionalUnit<K, V>>>();
+    }
+
+
+    public void Begin(final int transactionId) {
+
+        //Open a transaction with this id. Throw an exception if this ID already exists
+        if (transactionQueue.get(transactionId) != null) {
+            throw new RuntimeException("Request to begin transaction with ID " + transactionId + " received more than once");
+        }
+
+        transactionQueue.put(transactionId, new ArrayList<TransactionalUnit<K, V>>());
+    }
+
+    public void write(K key, V value, final int transactionId) {
+
+        //payload
+        //store.put(key, value);
+        List<TransactionalUnit<K, V>> transactionList = transactionQueue.get(transactionId);
+        if (transactionList == null) {
+            throw new RuntimeException("Attempting to add a write " +
+                    "operation to a transaction for which there is no entry " + transactionId);
+        }
+
+        TransactionalUnit<K, V> writeRequest = new ValueChange<K, V>(key, value);
+        transactionList.add(writeRequest);
+    }
+
+    public void read(K key, int transactionId) {
+
+        List<TransactionalUnit<K, V>> transactionList = transactionQueue.get(transactionId);
+        if (transactionList == null) {
+            throw new RuntimeException("Attempting to add a write " +
+                    "operation to a transaction for which there is no entry " + transactionId);
+        }
+
+        TransactionalUnit<K, V> writeRequest = new IsolatedRead<K, V>(key);
+        transactionList.add(writeRequest);
+    }
+
+
+    //In theory, Commit could return a set of Key-Values read
+    public List<TransactionalUnit<K,V>> Commit(final int transactionId) {
+
+        List<TransactionalUnit<K, V>> returnedResults = new ArrayList<TransactionalUnit<K, V>>();
+        for (TransactionalUnit<K, V> individualCommit : transactionQueue.get(transactionId)) {
+
+            K key = individualCommit.getKey();
+            if (individualCommit instanceof IsolatedRead) {
+
+                //a read. populate the read object
+                ((IsolatedRead) individualCommit).setValue(store.get(key));
+                returnedResults.add(individualCommit);
+            } else if (individualCommit instanceof ValueChange) {
+                //a write. Write it out.
+                store.put(key, individualCommit.getValue());
+                returnedResults.add(individualCommit);
+            }
+        }
+        return returnedResults;
+    }
+
+    public abstract static class TransactionalUnit<K, V> {
+
+        abstract K getKey();
+        abstract V getValue();
+    }
+
+    public static class IsolatedRead<K, V> extends TransactionalUnit {
+
+        private K key;
+        private V value;
+
+        public IsolatedRead(K key) {
+
+            this.key = key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public K getKey() { return this.key; }
+
+        Object getValue() {
+            return this.value;
+        }
+    }
+
+    public static class ValueChange<K, V> extends TransactionalUnit {
+
+        private K key;
+        private V value;
+
+        public ValueChange(K key, V newValue) {
+
+            this.key = key;
+            this.value = newValue;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+    }
+}
---
+++ src/test/java/TestTransactionStore.java
@@ -0,0 +1,117 @@
+import org.junit.*;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by aleks on 10/4/15.
+ */
+public class TestTransactionStore<K,V> {
+
+    private static final Logger logger =
+            Logger.getLogger(TestTransactionStore.class.getName());
+
+    static {
+        logger.setLevel(Level.INFO);
+    }
+
+    @Before
+    public void testSetup() {
+
+        logger.info("Starting test...");
+    }
+
+    @After
+    public void testTeardown() {
+
+
+        logger.info("Ending test...");
+    }
+
+
+    //Here's a question - how are we supposed to get intermediate values back from Read if this is one transaction and
+    // nothing actually gets executed until the commit?
+
+    // Either the append is done server-side, which would make it more like a trigger/coprocessor, or
+    // Otherwise, we'd need to somehow stop, mid-transaction, get the new value, and then continue. Also not
+    // a realistic possibility
+
+    // Maybe you can send it back, but then not allow any transactions to execute that have that value as a key??
+    // What if the client takes forever? We can add a reasonable timeout. Might involve a callback. Ugly.
+
+    // But it's super-ugly to have a transaction span multiple machines
+    @Test
+    @Ignore
+    public void testSimultaneousAppends() {
+
+        TransactionalKVStore<String,Integer> kvStore = new TransactionalKVStore<String,Integer>();
+        //TODO: Finish this when implementing returns from a read operation that hasn't been committed
+    }
+
+    @Test
+    public void testBasicReadWrite() {
+
+        TransactionalKVStore<String,Integer> kvStore = new TransactionalKVStore<String,Integer>();
+
+        final String KEY1 = "key1";
+        final int VALUE1 = 5;
+
+        int transactionId = 0;
+        kvStore.Begin(transactionId);
+        kvStore.write(KEY1, VALUE1, transactionId);
+        kvStore.read(KEY1, transactionId);
+
+        List<TransactionalKVStore.TransactionalUnit<String,Integer>> committedTransactions = kvStore.Commit(transactionId);
+        TransactionalKVStore.TransactionalUnit<String,Integer> readOutput = committedTransactions.get(1);
+        Assert.assertEquals((Integer)VALUE1, (Integer)readOutput.getValue());
+    }
+
+    @Test
+    // Verify that a multi-read transaction conducted at the same time as an atomic set of writes on those values produces
+    // results that are on consistent
+    public void testAtomicWrites() {
+
+        TransactionalKVStore<String,Integer> kvStore = new TransactionalKVStore<String,Integer>();
+
+        final int INITIAL_WRITE_TRANSACTION = 0;
+        final int INITIAL_READ_TRANSACTION = INITIAL_WRITE_TRANSACTION + 1;
+        final int INCREMENT_TRANSACTION = INITIAL_READ_TRANSACTION + 1;
+
+        final int INCREMENT = 25;
+
+        final String KEY1 = "key1";
+        final String KEY2 = "key2";
+        final Integer VALUE1_0 = 5;
+        final Integer VALUE2_0 = 5;
+
+        final Integer VALUE1_1 = VALUE1_0 + INCREMENT;
+        final Integer VALUE2_1 = VALUE2_0 + INCREMENT;
+
+        kvStore.Begin(INITIAL_WRITE_TRANSACTION);
+        kvStore.write(KEY1, VALUE1_0, INITIAL_WRITE_TRANSACTION);
+        kvStore.write(KEY2, VALUE2_0, INITIAL_WRITE_TRANSACTION);
+        kvStore.Commit(INITIAL_WRITE_TRANSACTION);
+
+        //Begin the read transaction but don't commit it!
+        kvStore.Begin(INITIAL_READ_TRANSACTION);
+        kvStore.read(KEY1, INITIAL_READ_TRANSACTION);
+        kvStore.read(KEY2, INITIAL_READ_TRANSACTION);
+
+        kvStore.Begin(INCREMENT_TRANSACTION);
+        kvStore.write(KEY1, VALUE1_1, INCREMENT_TRANSACTION);
+        kvStore.write(KEY2, VALUE2_1, INCREMENT_TRANSACTION);
+        kvStore.Commit(INCREMENT_TRANSACTION);
+
+        List<TransactionalKVStore.TransactionalUnit<String, Integer>> readResults = kvStore.Commit(INITIAL_READ_TRANSACTION);
+
+        final Integer READ_1_OUTPUT = readResults.get(0).getValue();
+        final Integer READ_2_OUTPUT = readResults.get(1).getValue();
+
+        //Verify that the values are the same and that the transaction held
+        Assert.assertEquals("Read different values", READ_1_OUTPUT, READ_2_OUTPUT);
+
+        //Verify that we pull down the incremented values and not the original, signifying that the commit order was honored
+        Assert.assertEquals("Read different values", READ_1_OUTPUT, VALUE2_1);
+    }
+}
